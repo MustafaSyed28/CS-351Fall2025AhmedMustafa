2666716bd381846e50bb654db6623ca459a98f2d
Syed Mustafa <syed.mustafa0328@gmail.com>
2025-10-29 15:22:14 -0500

    Enemy Object (Animation & Destroy)

--- CS351AhmedPrototype1/Assets/scripts/Projectile.cs
+++ CS351AhmedPrototype1/Assets/scripts/Projectile.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-
     private Rigidbody2D rb;
-
     public float speed = 20f;
+    public int damage = 20;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
-
         rb.velocity = transform.right * speed;
+    }
 
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
 
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
 
+        if (hitInfo.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }

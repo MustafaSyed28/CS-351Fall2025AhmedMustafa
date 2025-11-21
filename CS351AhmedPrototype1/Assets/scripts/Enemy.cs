13e6e86af931961162b33ba3a3c0ec0fe773e5cf
Syed Mustafa <syed.mustafa0328@gmail.com>
2025-11-21 11:32:11 -0600

    Enemy Walk Animation

--- CS351AhmedPrototype1/Assets/scripts/Enemy.cs
+++ CS351AhmedPrototype1/Assets/scripts/Enemy.cs
@@ -1,70 +1,81 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
-    public int health = 100;
-
-    public GameObject deathEffect;
-
+    //reference to healthbar
     private DisplayBar healthBar;
 
+    //Enemy's Health
+    public int health = 100;
+
+    //Prefab to spawn when enemy dies
+    public GameObject deathEffect;
+
+    //damage enemy deals to player
     public int damage = 10;
 
     private void Start()
     {
+        //find healthbar in children of enemy
         healthBar = GetComponentInChildren<DisplayBar>();
 
         if (healthBar == null)
         {
-            Debug.LogError("HealthBar (Display script) not found");
+            //if the health bar is not found, tag as error
+            Debug.LogError("HealthBar (DisplayBar Script) not found");
             return;
         }
-
         healthBar.SetMaxValue(health);
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
-
-            if (playerHealth == null)
-            {
-                Debug.LogError("PlayerHealth script not found on player");
-                return;
-            }
-
-            playerHealth.TakeDamage(damage);
-
-            playerHealth.Knockback(transform.position);
-        }
-    }
-
     public void TakeDamage(int damage)
     {
+        //subtract the damage dealt from the health
         health -= damage;
 
+        //update health bar 
         healthBar.SetValue(health);
 
+        //If health is less than or equal to 0, enemy dies :(
         if (health <= 0)
         {
+            //call die function
             Die();
         }
-
     }
 
     void Die()
     {
+        //Spawn death effect
         Instantiate(deathEffect, transform.position, Quaternion.identity);
 
+        //destroy the enemy
         Destroy(gameObject);
     }
 
+    //Damage player when they collide with enemy
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Get the player health script from player object
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+
+            //check if player health script is null
+            if (playerHealth == null)
+            {
+                Debug.LogError("Player Health script not found on player");
+                return;
+            }
+
+            //damage player
+            playerHealth.TakeDamage(damage);
+
+            //Knockback the player
+            playerHealth.Knockback(transform.position);
+        }
+    }
+
 }
-
-
-
--- CS351AhmedPrototype1/Assets/scripts/PlayerHealth.cs
+++ CS351AhmedPrototype1/Assets/scripts/PlayerHealth.cs
@@ -1,120 +1,148 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    //variable to store health of player
     public int health = 100;
 
+    //reference to healthbar, must be set in inspector
     public DisplayBar healthBar;
 
+    //reference to player's rigidbody2d, needed for knockback
     private Rigidbody2D rb;
 
+    //variable to track knockback force when  player collides with an enemy
     public float knockbackForce = 5f;
 
+    //prefab to spawn when player dies, must be set in inspector
     public GameObject playerDeathEffect;
 
+    //bool to keep track of if the player was hit recently
     public static bool hitRecently = false;
 
-    public float hitRecoveryTime = 0.2f;
+    //Time in seconds to recover from hit;
+    public float hitRecoveryTime;
 
-    private AudioSource playerAudio;
-    public AudioClip playerHitSound;
-    public AudioClip playerDeathSound;
-
+    //reference for animator
     private Animator animator;
 
+    //References for sound effects
+    private AudioSource playerAudio;
+    public AudioClip playerHitSound;
     // Start is called before the first frame update
     void Start()
     {
+        //Set animator reference
         animator = GetComponent<Animator>();
 
+        //Set Audio Source Reference
         playerAudio = GetComponent<AudioSource>();
 
+        //set rigidbody2d reference
         rb = GetComponent<Rigidbody2D>();
 
+        //check if rigidbody2d reference is null
         if (rb == null)
         {
-            Debug.LogError("Rigidbody2D not found on player");
+            Debug.LogError("Rigidbody2D is not found on player");
         }
 
+        //set the healthbar max value to the player's health
         healthBar.SetMaxValue(health);
 
+        //initialize hitRecently to false
         hitRecently = false;
-
     }
-
+    //function to knock player back when they collide with enemy
     public void Knockback(Vector3 enemyPosition)
     {
+        // If the player has been hit recently 
         if (hitRecently)
-        {
+        {//return out of function
             return;
         }
 
+        //set hitRecently to true
         hitRecently = true;
 
         if (gameObject.activeSelf)
         {
-
+            //Start coroutine to reset HitRecently
             StartCoroutine(RecoverFromHit());
-
         }
 
-
+        //calcualte the direction of knockback
         Vector2 direction = transform.position - enemyPosition;
 
+        //Normalize the direction vector
+        //This gives consistent knockback force regardless of the distance between the player and enemy
         direction.Normalize();
 
+        //Add upward direction to knockback
         direction.y = direction.y * 0.5f + 0.5f;
 
+        //Add forec to the player in the direction of the knockback
         rb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
-
-        
     }
 
+    //Corutine to reset hitRecently after hitRecoveryTime seconds
     IEnumerator RecoverFromHit()
     {
+        //wait for hitRecoveryTime (0.2) seconds
         yield return new WaitForSeconds(hitRecoveryTime);
 
+        //Set hitRecently to false
         hitRecently = false;
 
+        //set hit animation to false
         animator.SetBool("hit", false);
     }
-
     public void TakeDamage(int damage)
     {
+        //Subtract the damage from the health
         health -= damage;
 
+        //update healthbar
         healthBar.SetValue(health);
 
+        //TODO: Play an animation to when the player takes damage
+
+        //if health is less than or equal to 0
         if (health <= 0)
         {
             Die();
         }
         else
         {
+            //Play playerHitSound
             playerAudio.PlayOneShot(playerHitSound);
 
+            //Play player hurt animator
             animator.SetBool("hit", true);
         }
-
     }
 
-    public void Die()
+    void Die()
     {
         ScoreManager.gameOver = true;
 
-        //playerAudio.PlayOneShot(playerDeathSound);
-
+        //Instantiate the death effect at the player's position
         GameObject deathEffect = Instantiate(playerDeathEffect, transform.position, Quaternion.identity);
 
+        //Destroy the death effect after 2 seconds
+        //Destroy(deathEffect, 2f); <-Not needed because of Destroy after delay script
+
+        //disable player object
         gameObject.SetActive(false);
     }
-            
-
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
+
